9d9c37305df15cde740d582cca68ef52fb19160c
mnoeel <mnoeel@oakland.edu>
2023-10-27 22:20:51 -0400

    shuffle outfit buttons

--- app/src/main/java/com/example/styleshuffle/ui/dashboard/ShuffleFragment.java
+++ app/src/main/java/com/example/styleshuffle/ui/dashboard/ShuffleFragment.java
@@ -39,6 +39,7 @@ public class ShuffleFragment extends Fragment {
     TopItemDAO topItemDAO;
     ShoeItemDAO shoeItemDAO;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_shuffle, container, false);
